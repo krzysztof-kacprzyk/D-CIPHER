46fff6cd84d67b1b7ff61c2be641b21195485e9a
krzysztof-kacprzyk <kr.kacprzyk@gmail.com>
2022-03-21 22:00:06 +0000

    Add logging to run_var_square

--- var_objective/run_var_square.py
+++ var_objective/run_var_square.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import numpy as np
 import argparse
 import time
@@ -31,6 +32,20 @@ def _check_if_zero(vector):
     else:
         return False
 
+def save_output(filename, trial, seed, program, operator, loss, target_loss, target_loss_better_weights, time_elapsed):
+    message = f"""
+----------------------
+Trial: {trial}
+Seed: {seed}
+Program: {program}
+Operator: {operator}
+Loss: {loss}
+Target_loss: {target_loss}
+Target_loss_better_weights: {target_loss_better_weights}
+Time elapsed: {time_elapsed}"""
+    with open(filename, 'a') as f:
+        f.write(message)
+
 
 if __name__ == '__main__':
 
@@ -60,102 +75,124 @@ if __name__ == '__main__':
 
     conditions = get_conditions_set(args.conditions_set)
 
-    print(f"Seed set to {args.seed}")
-    print(f"Generating dataset of {args.name} on a grid with width {args.width}, frequency per dim {args.frequency_per_dim}, noise ratio {args.noise_ratio} and using conditions set {args.conditions_set}")
-    start = time.time()
-    observed_dataset = generate_fields(pdes, conditions, observed_grid, args.noise_ratio, seed=args.seed)
-    end = time.time()
-    print(f"Observed dataset generated in {end-start} seconds")
-
-    print(f"Estimating fields on a grid with frequency {args.full_grid_samples} per dimension")
-    start = time.time()
-    full_dataset = estimate_fields(observed_grid,observed_dataset,full_grid,seed=args.seed)
-    end = time.time()
-    print(f"Fields estimated in {end-start} seconds")
-
-    dimension = pdes.get_expression()[args.field_index][0].dimension
-    order = pdes.get_expression()[args.field_index][0].order
-
-    if args.basis == 'fourier':
-        basis = FourierSine2D(widths)
-        index_limits = [args.max_ind_basis] * 2
-    elif args.basis == '2spline2D':
-        basis = BSplineFreq2D(widths, 2)
-        index_limits = [args.max_ind_basis] * 2
-
-    print("Initializing Variational Weights Finder")
-    start = time.time()
-    var_wf = VariationalWeightsFinder(full_dataset,args.field_index,full_grid,dimension=dimension,order=order,basis=basis,index_limits=index_limits,optim_engine='svd',seed=args.seed)
-    end = time.time()
-    print(f"Weight Finder initialized in {end-start} seconds")
-
-
-    def _var_fitness(y, y_pred, w):
-
-        if len(y_pred) == 2:
-            return 0.0
-
-        if _check_if_zero(y_pred):
-            loss, weights = var_wf.find_weights(None)
-        else:
-            loss, weights = var_wf.find_weights(y_pred)
-
-        if loss is None:
-            return INF
-
-        return loss
-    
-    X = grid_and_fields_to_covariates(var_wf.grid_and_fields)
-    fake_y = np.zeros(X.shape[0])
-
-    var_fitness = make_fitness(_var_fitness, greater_is_better=False)
+    dt = datetime.now().strftime("%d-%m-%YT%H.%M.%S")
+    filename = f"results/run_var_square_{dt}"
 
     gp_params = get_gp_params()
 
-    L_target, g_target = pdes.get_expression_normalized()[args.field_index]
-    target_weights = L_target.get_adjoint().vectorize()[1:] # exclude zero-order partial
-    target_g_numpy = pdes.numpify_g(g_target)
-    variables_part = [X[:,i] for i in range(pdes.M+pdes.N)]
-
-    target_g_part = target_g_numpy(*variables_part)
-
-    # Check if target_g_part is an array
-    if not hasattr(target_g_part, "__len__"):
-        # This means that it is a scalar
-        target_g_part = float(target_g_part)
-        if target_g_part == 0.0:
-            target_g_part = None
-        else:
-            target_g_part = np.ones(X.shape[0]) * target_g_part
-            # TODO: maybe in the future leverage the fact that it is a scalar
+    with open(filename, 'w') as f:
+        f.write(f"""
+Global seed: {args.seed}
+Parameters: {args}
+gplearn config: {gp_params}
+        """)
     
-    target_loss = var_wf._calculate_loss(target_g_part, target_weights)
-    print(f"Loss with target weights and target g_part: {target_loss}")
-    print(f"Target weights: {target_weights}")
+    if args.num_trials == 0:
+        seeds = [args.seed]
+    else:
+        np.random.seed(args.seed)
+        seeds = np.random.randint(0,1000000,size=args.num_trials)
 
-    best_found_loss, best_found_weights = var_wf.find_weights(target_g_part)
-    print(f"Loss for the best found weights: {best_found_loss}")
-    print(f"Best found weights: {best_found_weights}")
+    for trial, seed in enumerate(seeds):
 
-    print(f"Starting evolution with population {gp_params['population_size']} and {gp_params['generations']} generations")
-    start = time.time()
-    est = SymbolicRegressor(metric=var_fitness, **gp_params ,verbose=1, random_state=args.seed)
+        print(f"Seed set to {seed}")
+        print(f"Generating dataset of {args.name} on a grid with width {args.width}, frequency per dim {args.frequency_per_dim}, noise ratio {args.noise_ratio} and using conditions set {args.conditions_set}")
+        start = time.time()
+        observed_dataset = generate_fields(pdes, conditions, observed_grid, args.noise_ratio, seed=seed)
+        end = time.time()
+        print(f"Observed dataset generated in {end-start} seconds")
 
-    est.fit(X, fake_y)
+        print(f"Estimating fields on a grid with frequency {args.full_grid_samples} per dimension")
+        start = time.time()
+        full_dataset = estimate_fields(observed_grid,observed_dataset,full_grid,seed=seed)
+        end = time.time()
+        print(f"Fields estimated in {end-start} seconds")
 
-    loss, weights = var_wf.find_weights(est.predict(X))
+        dimension = pdes.get_expression()[args.field_index][0].dimension
+        order = pdes.get_expression()[args.field_index][0].order
 
-    linear_operator = LinearOperator.from_vector(weights, dimension, order, zero_partial=False)
+        if args.basis == 'fourier':
+            basis = FourierSine2D(widths)
+            index_limits = [args.max_ind_basis] * 2
+        elif args.basis == '2spline2D':
+            basis = BSplineFreq2D(widths, 2)
+            index_limits = [args.max_ind_basis] * 2
 
-  
-    try:
-        eq, eqC = gp_to_pysym_with_coef(est)
-    except:
-        eq = est._program
+        print("Initializing Variational Weights Finder")
+        start = time.time()
+        var_wf = VariationalWeightsFinder(full_dataset,args.field_index,full_grid,dimension=dimension,order=order,basis=basis,index_limits=index_limits,optim_engine='svd',seed=seed)
+        end = time.time()
+        print(f"Weight Finder initialized in {end-start} seconds")
 
-    print(f"Found {linear_operator.get_adjoint()} - ({eq}) = 0")
 
-    print(f"Expected: {L_target} - ({g_target}) = 0")
+        def _var_fitness(y, y_pred, w):
 
-    end = time.time()
-    print(f"Evolution finished in {end-start} seconds")
+            if len(y_pred) == 2:
+                return 0.0
+
+            if _check_if_zero(y_pred):
+                loss, weights = var_wf.find_weights(None)
+            else:
+                loss, weights = var_wf.find_weights(y_pred)
+
+            if loss is None:
+                return INF
+
+            return loss
+        
+        X = grid_and_fields_to_covariates(var_wf.grid_and_fields)
+        fake_y = np.zeros(X.shape[0])
+
+        var_fitness = make_fitness(_var_fitness, greater_is_better=False)
+
+       
+
+        L_target, g_target = pdes.get_expression_normalized()[args.field_index]
+        target_weights = L_target.get_adjoint().vectorize()[1:] # exclude zero-order partial
+        target_g_numpy = pdes.numpify_g(g_target)
+        variables_part = [X[:,i] for i in range(pdes.M+pdes.N)]
+
+        target_g_part = target_g_numpy(*variables_part)
+
+        # Check if target_g_part is an array
+        if not hasattr(target_g_part, "__len__"):
+            # This means that it is a scalar
+            target_g_part = float(target_g_part)
+            if target_g_part == 0.0:
+                target_g_part = None
+            else:
+                target_g_part = np.ones(X.shape[0]) * target_g_part
+                # TODO: maybe in the future leverage the fact that it is a scalar
+        
+        target_loss = var_wf._calculate_loss(target_g_part, target_weights)
+        print(f"Loss with target weights and target g_part: {target_loss}")
+        print(f"Target weights: {target_weights}")
+
+        best_found_loss, best_found_weights = var_wf.find_weights(target_g_part)
+        print(f"Loss for the best found weights: {best_found_loss}")
+        print(f"Best found weights: {best_found_weights}")
+
+        print(f"Starting evolution with population {gp_params['population_size']} and {gp_params['generations']} generations")
+        start = time.time()
+        est = SymbolicRegressor(metric=var_fitness, **gp_params ,verbose=1, random_state=seed)
+
+        est.fit(X, fake_y)
+
+        loss, weights = var_wf.find_weights(est.predict(X))
+
+        linear_operator = LinearOperator.from_vector(weights, dimension, order, zero_partial=False)
+
+    
+        try:
+            eq, eqC = gp_to_pysym_with_coef(est)
+        except:
+            eq = est._program
+
+        print(f"Found {linear_operator.get_adjoint()} - ({eq}) = 0")
+
+        print(f"Expected: {L_target} - ({g_target}) = 0")
+
+        end = time.time()
+        print(f"Evolution finished in {end-start} seconds")
+
+        save_output(filename, trial+1, seed, eq, linear_operator.get_adjoint(), loss, target_loss, best_found_loss, end-start)
